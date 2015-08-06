bee88503da1cfce3e2926ae70ee6e3b80bff667d
John Hampton <john_j_hampton@yahoo.com>
2015-08-06 00:15:42 -0400

    Create main.js in new scripts directory

--- signup.php
+++ signup.php
@@ -9,4 +9,27 @@ if (isset($_REQUEST['email'])) {
 }
 
 echo $email;
+// Define MySQL connection and credentials
+$pdo_dsn='mysql:dbname=yourdatabase;host=databasehost.example.com';
+$pdo_user='yourdatabaseuser';
+$pdo_password='yourdatabaspassword';
+
+try {
+    // Establish connection to database
+    $conn = new PDO($pdo_dsn, $pdo_user, $pdo_password);
+
+    // Throw exceptions in case of error.
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Use prepared statements to mitigate SQL injection attacks.
+    // See http://stackoverflow.com/questions/60174/how-can-i-prevent-sql-injection-in-php for more details
+    $qry=$conn->prepare('INSERT INTO yourtable (yourcolumn) VALUES (:yourvalue)');
+
+    // Execute the prepared statement using user supplied data.
+    $qry->execute(Array(":yourvalue" => $yourfield));
+
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage() . " file: " . $e->getFile() . " line: " . $e->getLine();
+    exit;
+}
 ?>
